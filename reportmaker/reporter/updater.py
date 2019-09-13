35059828a4843890c7b94f15b011c0e88e6c2f8c
Hugant Mirron <Hugant@users.noreply.github.com>
2019-09-13 17:53:27 +0700

    Style fixes

--- reportmaker/reporter/updater.py
+++ reportmaker/reporter/updater.py
@@ -1,5 +1,8 @@
+from datetime import timedelta, datetime
+
 import OpenOPC
 import apscheduler
+import pytz
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
@@ -12,35 +15,41 @@ scheduler = BackgroundScheduler()
 
 def start():
 	pass
-	# if scheduler.state == apscheduler.schedulers.base.STATE_RUNNING:
-	# 	scheduler.shutdown()
-	#
-	# scheduler.remove_all_jobs()
-	# reports = Report.objects.all()
-	# for report in reports:
-	# 	print("Job will be created by id " + str(report.id))
-	# 	scheduler.add_job(update, 'interval', args=[report.id], minutes=report.interval_update, id=str(report.id))
-	#
-	# scheduler.start()
+	if scheduler.state == apscheduler.schedulers.base.STATE_RUNNING:
+		scheduler.shutdown()
+
+	scheduler.remove_all_jobs()
+	reports = Report.objects.all()
+
+	for report in reports:
+		max_interval_time = datetime.now(tz=pytz.utc) - timedelta(minutes=report.interval_update)
+		processes = Process.objects.all().filter(report=report.id)
+		if (len(processes) == 0) or (processes.latest('date').date < max_interval_time):
+			update(report.id)
+
+		print("Job will be created by id " + str(report.id))
+		scheduler.add_job(update, 'interval', args=[report.id], minutes=report.interval_update, id=str(report.id))
+
+	scheduler.start()
 
 
-# def update(report_id):
-# 	try:
-# 		report = Report.objects.get(id=int(report_id))
-# 		opc = OpenOPC.client()
-# 		opc.connect('Matrikon.OPC.Simulation')
-# 		process = opc.read('Random.Int4')
-# 		process_name = report.title_text + " - " + "process #" + str(Process.objects.all().filter(report=report.id).distinct().count())
-# 		print(timezone.now())
-# 		process = Process(
-# 			report_id=report.id,
-# 			name_text=process_name,
-# 			state_text=process[1],
-# 			ram_in_kb_int=process[0],
-# 			date=timezone.now())
-# 		process.save()
-# 		print(process)
-# 		report.process_set.add(process)
-# 		print("created")
-# 	except ObjectDoesNotExist:
-# 		scheduler.remove_job(report_id)
+def update(report_id):
+	try:
+		report = Report.objects.get(id=int(report_id))
+		opc = OpenOPC.client()
+		opc.connect('Matrikon.OPC.Simulation')
+		process = opc.read('Random.Int4')
+		process_name = report.title_text + " - " + "process #" + str(Process.objects.all().filter(report=report.id).distinct().count())
+		print(timezone.now())
+		process = Process(
+			report_id=report.id,
+			name_text=process_name,
+			state_text=process[1],
+			ram_in_kb_int=process[0],
+			date=timezone.now())
+		process.save()
+		print(process)
+		report.process_set.add(process)
+		print("created")
+	except ObjectDoesNotExist:
+		scheduler.remove_job(report_id)
