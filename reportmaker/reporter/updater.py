25ea6cfe34660572836a7c44f4783ddfd15df961
Hugant Mirron <Hugant@users.noreply.github.com>
2019-09-22 01:21:33 +0700

    Added logging

--- reportmaker/reporter/updater.py
+++ reportmaker/reporter/updater.py
@@ -15,6 +15,7 @@ scheduler = BackgroundScheduler()
 
 
 def start():
+	logging.info("Background scheduler on start")
 	if scheduler.state == apscheduler.schedulers.base.STATE_RUNNING:
 		scheduler.shutdown()
 
@@ -27,19 +28,20 @@ def start():
 		if (len(processes) == 0) or (processes.latest('date').date < max_interval_time):
 			update(report.id)
 
-		print("Job will be created by id " + str(report.id))
 		scheduler.add_job(update, 'interval', args=[report.id], minutes=report.interval_update, id=str(report.id))
+		logging.info("Job will be created by id " + str(report.id))
 
 	scheduler.start()
+	logging.info("Background scheduler started")
 
 
 def update(report_id):
-	logging.debug("Trying update report " + str(report_id))
+	logging.info("Trying update report " + str(report_id))
 	try:
 		report = Report.objects.get(id=int(report_id))
 		opc = OpenOPC.client()
 		opc.connect('Matrikon.OPC.Simulation')
-		logging.debug("OPC connect completed")
+		logging.info("OPC connect completed")
 		process = opc.read('Random.Int4')
 		process_name = report.title_text + " - " + "process #" + str(Process.objects.all().filter(report=report.id).distinct().count())
 		process = Process(
@@ -50,9 +52,7 @@ def update(report_id):
 			date=timezone.now())
 		process.save()
 		report.process_set.add(process)
-		logging.debug("Process has been created by name " + process_name)
-		print("Created process " + process_name)
+		logging.info("Process has been created by name " + process_name)
 	except ObjectDoesNotExist:
-		logging.debug("Remove job from scheduler by id " + report_id)
+		logging.info("Remove job from scheduler by id " + report_id)
 		scheduler.remove_job(report_id)
-		print("Removed job by id " + str(report_id))
--- reportmaker/reportmaker/settings.py
+++ reportmaker/reportmaker/settings.py
@@ -100,6 +100,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
 LOGIN_URL = '/login'
 
 # Internationalization
